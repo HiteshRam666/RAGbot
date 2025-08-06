a4369edfde7613bc5d029b30af6e90d3c6329118
hitesh <hiteshram321@gmail.com>
2025-08-06 23:14:10 +0530

    Setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Finance Chatbot", 
+    version="0.1.0", 
+    author="Hitesh Ram", 
+    author_email="hiteshram321@gmail.com",
+    packages=find_packages(), 
+    install_requires = []
+)
